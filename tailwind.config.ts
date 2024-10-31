d455124f36b26556cc32ff427198b62d0b23c7e0
mahdi sadeghi <mahdisadeghimsm1376@gmail.com>
2024-10-31 17:35:21 +0330

    font and color setting

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,41 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode:'class',
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      'sm': '640px',
+      'md': '840px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
+    fontFamily: {
+      'iran_Bold': 'iran_Bold',
+      'iran_Light': 'iran_Light',
+    },
     extend: {
+      fontSize: {
+        clamp: "clamp(0.7rem, 1vw, 1rem)",
+        clamp_intro: "clamp(1.5rem, 2vw, 2rem)",
+        clamp_des: "clamp(1rem, 1.2vw, 2rem)",
+        clamp_super_des: "clamp(0.9rem, 1vw, 2rem)",
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        green_snap: '#21aa58',
+        text_nav: '#414141',
+        bg_gray: '#f2f5f8',
+        text_bg: '#404040',
+        text_muted: '#eee',
+        bg_muted: 'rgba(255,255,255,0.05)',
+        bg_nav_res: 'rgba(0,0,0,0.5)',
+        bg_dark1: '#242424',
+        bg_dark2: '#000',
       },
     },
   },
